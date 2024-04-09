b0f077cf0ca159acc992a7236897387b504ef1df
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-09 13:11:41 +0530

    Styling through Styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,10 @@ import styled from "styled-components";
 import GlobalStyles from "../styles/GlobalStyles";
 import Button from "../ui/Button";
 import Input from "../ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -19,10 +14,23 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Input type="text" placeholder="Enter your name" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button>Check in</Button>
+              <Button>Check out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="text" placeholder="Number of guests" />
+              <Input type="text" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
