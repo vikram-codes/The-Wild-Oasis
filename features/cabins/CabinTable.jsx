42f24fe52bc5b7b5b7a073b7151c2c4abf179282
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-10 13:51:26 +0530

    Displaying Rows to showcase fetched data from supabase using react-query

--- features/cabins/CabinTable.jsx
+++ features/cabins/CabinTable.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useQuery } from "@tanstack/react-query";
 import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -40,7 +41,21 @@ function CabinTable() {
     return <Spinner />;
   }
 
-  return <div>Table</div>;
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  );
 }
 
 export default CabinTable;
