4de755d9d96371252b547384f12727120c14bbbf
Vikram Saini <vikramjit.personal@gmail.com>
2024-04-21 17:20:11 +0530

    fetching cabins and guests data in apiBookings

--- services/apiBookings.js
+++ services/apiBookings.js
@@ -2,7 +2,9 @@ import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
 export async function getBookings() {
-  let { data, error } = await supabase.from("bookings").select("*");
+  let { data, error } = await supabase
+    .from("bookings")
+    .select("*, cabins(*), guests(*)");
 
   if (error) {
     console.error(error);
