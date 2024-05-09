9ef694cda0eb6ea8a755d0f2164eade25f167ebf
Vikram Saini <vikramjit.personal@gmail.com>
2024-05-09 16:29:44 +0530

    Adding checked out button for checked in customers

--- features/bookings/BookingRow.jsx
+++ features/bookings/BookingRow.jsx
@@ -7,7 +7,7 @@ import Menus from "../../ui/Menus";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
-import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 
 const Cabin = styled.div`
@@ -99,6 +99,14 @@ function BookingRow({
               onClick={() => navigate(`/checkin/${bookingId}`)}
             >
               Check in
+            </Menus.Button>
+          )}
+          {status === "checked-in" && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare />}
+              onClick={() => navigate(`/checkin/${bookingId}`)}
+            >
+              Check out
             </Menus.Button>
           )}
         </Menus.List>
