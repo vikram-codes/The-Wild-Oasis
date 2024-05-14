806acbec8874597318484d5c1ed873fc5a5568cf
Vikram Saini <vikramjit.personal@gmail.com>
2024-05-14 14:40:55 +0530

    Creating a login form for users

--- features/authentication/LoginForm.jsx
+++ features/authentication/LoginForm.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
-import FormRow from "../../ui/FormRow";
+import FormRowVertical from "../../ui/FormRowVertical";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
@@ -12,7 +12,7 @@ function LoginForm() {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address">
+      <FormRowVertical label="Email address">
         <Input
           type="email"
           id="email"
@@ -21,8 +21,8 @@ function LoginForm() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-      </FormRow>
-      <FormRow label="Password">
+      </FormRowVertical>
+      <FormRowVertical label="Password">
         <Input
           type="password"
           id="password"
@@ -30,10 +30,10 @@ function LoginForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-      </FormRow>
-      <FormRow>
+      </FormRowVertical>
+      <FormRowVertical>
         <Button size="large">Login</Button>
-      </FormRow>
+      </FormRowVertical>
     </Form>
   );
 }
