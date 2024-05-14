6b64fc07f8e4d1dcd8058ce137f121ed2f10adec
Vikram Saini <vikramjit.personal@gmail.com>
2024-05-14 15:41:42 +0530

    Authenticating credentials of user

--- ui/ProtectedRoute.jsx
+++ ui/ProtectedRoute.jsx
@@ -1,12 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "../ui/Spinner";
+import { useEffect } from "react";
+
 function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
   //1. Load the authenticated user
+  const { user, isLoading, isAuthenticated } = useUser();
 
-  //2. While loading, show a loading spinner
+  //2. If the user is not authenticated, redirect to the login page
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate("/login");
+  }, [isAuthenticated, isLoading]);
 
-  //3. If the user is not authenticated, redirect to the login page
+  //3. While loading, show a loading spinner
+  if (isLoading) {
+    return (
+      <div>
+        <Spinner />
+      </div>
+    );
+  }
 
   //4. If the user is authenticated, render the app
-  return children;
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
