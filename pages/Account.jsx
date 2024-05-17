d1a714d6bdcb5ef95d183c0e7eb2d089a593137f
Vikram Saini <vikramjit.personal@gmail.com>
2024-05-17 19:44:14 +0530

    Updating New Password

--- pages/Account.jsx
+++ pages/Account.jsx
@@ -1,4 +1,5 @@
 import UpdateUserDataForm from "../features/authentication/UpdateUserDataForm";
+import UpdatePasswordForm from "../features/authentication/UpdatePasswordForm";
 
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
@@ -7,16 +8,15 @@ function Account() {
   return (
     <>
       <Heading as="h1">Update your account</Heading>
-      <UpdateUserDataForm />
 
       <Row>
         <Heading as="h3">Update user data</Heading>
-        <p>Update user data form</p>
+        <UpdateUserDataForm />
       </Row>
 
       <Row>
         <Heading as="h3">Update password</Heading>
-        {/* <UpdatePasswordForm /> */}
+        <UpdatePasswordForm />
       </Row>
     </>
   );
