da7c20b656f0677a071cb488df643253ce14746d
Vikram Saini <vikramjit.personal@gmail.com>
2024-05-17 16:56:20 +0530

    featuring header

--- ui/Header.jsx
+++ ui/Header.jsx
@@ -5,7 +5,10 @@ import UserAvatar from "../features/authentication/UserAvatar";
 const StyledHeader = styled.header`
   background-color: var(--color-);
   padding: 1.2rem 4.8rem;
-
+  display: flex;
+  gap: 2.5rem;
+  align-items: center;
+  justify-content: end;
   border-bottom: 1px solid var(--color-grey-100);
 `;
 
