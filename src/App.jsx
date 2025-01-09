73f4682e3cd663996f4a43a8ba1a34c4f3548efe
vikram-codes <vikramjit.personal@gmail.com>
2025-01-09 13:15:15 -0500

    Creating External styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
 `;
 
 function App() {
