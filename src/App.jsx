1b28ccd935cbec22d8bd1bef5f53e5e471ec02d8
vikram-codes <vikramjit.personal@gmail.com>
2025-01-08 20:34:01 -0500

    Creating Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ const H1 = styled.h1`
   font-weight: 600;
 `;
 
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
+
 function App() {
   return (
     <>
       <H1> Hello The-Wild-Oasis</H1>
+      <Button>Click Me</Button>
     </>
   );
 }
