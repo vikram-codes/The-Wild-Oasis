8ca857df2954eb63fad62c1303e5c86cb64d6667
vikram-codes <vikramjit.personal@gmail.com>
2025-01-09 14:02:45 -0500

    Finishing up the styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
+const StyledApp = styled.main`
+  padding: 20px;
 `;
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <H1> Hello The-Wild-Oasis</H1>
-      <Button>Click Me</Button>
+      <StyledApp>
+        <Row type="horizontal">
+          <Heading as="h1">The-Wild-Oasis</Heading>
+          <div>
+            <Button>Check-in</Button>
+            <Button>Check-out</Button>
+          </div>
+        </Row>
+        <Row type="vertical">
+          <form>
+            <Heading as="h3">Form</Heading>
+            <Input type="number" placeholder="Number of guests" />
+            <Input type="number" placeholder="Number of guests" />
+          </form>
+        </Row>
+      </StyledApp>
     </>
   );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -49,15 +49,17 @@ const variations = {
 };
 
 const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
   border: none;
-  border-radius: var() (--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
+  border-radius: var(--border-radius-sm);
   box-shadow: var(--shadow-sm);
-  cursor: pointer;
+
+  ${(props) => sizes[props.size]}
+  ${(props) => variations[props.variation]}
 `;
+
+Button.defaultProps = {
+  variation: "primary",
+  size: "medium",
+};
 
 export default Button;
