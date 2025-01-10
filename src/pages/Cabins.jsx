7dac6f4ef5dc043334cf6aa66708a80dd1335e31
vikram-codes <vikramjit.personal@gmail.com>
2025-01-10 14:10:19 -0500

    Setting up Supabase storage buckets

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -14,6 +14,10 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://khxmjwkjxklzwtfwzzkh.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg"
+        alt="Cabin"
+      />
     </Row>
   );
 }
