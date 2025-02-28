21e934dfdb76c4353918a3be8648e183cdb5ce4b
vikram-codes <vikramjit.personal@gmail.com>
2025-02-28 15:05:36 -0500

    Clied-Side Sortin: Sorting Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -11,8 +11,8 @@ function CabinTable() {
 
   if (isLoading) return <Spinner />;
 
+  // Filter cabins based on the discount query parameter
   const filterValue = searchParams.get("discount") || "all";
-  console.log(filterValue);
 
   let filterCabins;
   if (filterValue === "all") filterCabins = cabins;
@@ -20,6 +20,14 @@ function CabinTable() {
     filterCabins = cabins.filter((cabin) => cabin.discount === 0);
   if (filterValue === "with-discount")
     filterCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  // Sort cabins based on the sortBy query parameter
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  let sortedCabins = filterCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -34,7 +42,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filterCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
@@ -10,6 +11,19 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "no-discount", label: "No discount" },
           { value: "with-discount", label: "With discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Sort by Name (A to Z)" },
+          { value: "name-desc", label: "Sort by Name (Z to A)" },
+          { value: "regularPrice-asc", label: "Sort by Price (low to high)" },
+          { value: "regularPrice-desc", label: "Sort by Price (high to low)" },
+          { value: "maxCapacity-asc", label: "Sort by Capacity (low to high)" },
+          {
+            value: "maxCapacity-desc",
+            label: "Sort by Capacity (high to low)",
+          },
         ]}
       />
     </TableOperations>
