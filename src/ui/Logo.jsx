845a7c93fc9ed3ce7f3fbc511d7ff4743808db1b
vikram-codes <vikramjit.personal@gmail.com>
2025-05-15 18:32:01 -0400

    Fixing small bugs

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import useDarkMode from "../context/DarkModeContext";
+import useDarkMode from "../../src/hooks/useDarkMode";
 
 const StyledLogo = styled.div`
   text-align: center;
