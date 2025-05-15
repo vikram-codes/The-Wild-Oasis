87540018ce2c6728317d2d9ebe3ae15548bf786f
vikram-codes <vikramjit.personal@gmail.com>
2025-05-15 14:30:39 -0400

    Application Done

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,9 +6,7 @@ import Stats from "./Stats";
 import { useCabin } from "../cabins/useCabin";
 import SalesChart from "./SalesChart";
 import DurationChart from "./DurationChart";
-// import SalesChart from "./SalesChart";
-// import DurationChart from "./DurationChart";
-// import TodayActivity from "../check-in-out/TodayActivity";
+import TodayActivity from "../check-in-out/TodayActivity";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -32,6 +30,7 @@ function DashboardLayout() {
         numDays={numDays}
         cabinCount={cabins.length}
       />
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
